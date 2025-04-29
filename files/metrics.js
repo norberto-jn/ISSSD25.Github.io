e296e258e99b456abd89f9b6801285108ee14204
Norberto Ferreira <norbertojunio@gmail.com>
2025-04-28 21:04:06 -0300

    add google

--- files/metrics.js
+++ files/metrics.js
@@ -1,5 +1,5 @@
 window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
+function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-FR2CSEK720');
+gtag('config', 'G-FCJSH23Z6B');
